e3a72ffc1bb89b0744f7297721ff23f14de0c935
Cesvenbl <cedrik.svenblad@gymnasium.ax>
2017-01-26 14:28:44 +0200

    många ändringar
    använde bootstrap för att köra sidan snyggare och lade till lite mera
    text som boten kan säga

--- bot.php
+++ bot.php
@@ -3,6 +3,6 @@ header("Content-type: text/plain; charset=utf-8");
 if ($_GET['meme'] == NULL) {
 	echo "Hej!";
 } else {
-	$memes = ["Thanks Obama!","9+10=21","5/7 perfect score!"];
+	$memes = ["Thanks Obama!","9+10=21","5/7 perfect score!", "i'm going to build a WALL","No God Please noooooo!","never going to give you up" ,$_GET['meme']];
 	echo $memes[random_int(0, count($memes) - 1)];
 }
